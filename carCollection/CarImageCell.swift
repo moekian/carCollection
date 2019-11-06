453bca7a32743f538d31814a25e3ebc719ce9efb
Mohammad Kiani <mohammadkiani@outlook.com>
2019-11-06 18:56:24 -0500

    collection view demo

---
+++ carCollection/CarImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  CarImageCell.swift
+//  carCollection
+//
+//  Created by Mohammad Kiani on 2019-11-06.
+//  Copyright © 2019 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class CarImageCell: UICollectionViewCell {
+    @IBOutlet weak var carImageaView: UIImageView!
+    
+//    var carName: String! {
+//        didSet {
+//            carImageaView.image = UIImage(named: carName)
+//        }
+//    }
+}
--- carCollection/ViewController.swift
+++ carCollection/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    var carNames = ["car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return carNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "carCell", for: indexPath) as! CarImageCell
+//        cell.carName = carNames[indexPath.row]
+        cell.carImageaView.image = UIImage(named: carNames[indexPath.row])
+        return cell
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
